4c73725dd1b9ebda24f3a54f616b69e91208b72e
mayurmore14 <mayurmore8505@gmail.com>
2022-04-01 11:29:24 +0530

    ADDED: Player Position

---
+++ src/com/infogalaxy/SnakeAndLaddersSimulator.java
@@ -0,0 +1,13 @@
+package com.infogalaxy;
+
+public class SnakeAndLaddersSimulator {
+    public void position() {
+        int position = 0;
+        System.out.println("Players Position is = "+position);
+    }
+    public static void main(String[] args) {
+        SnakeAndLaddersSimulator player1 = new SnakeAndLaddersSimulator();
+
+        player1.position();
+    }
+}
