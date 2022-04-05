318be48205edf5f5eee5964f60cc91ea254c70e6
mayurmore14 <mayurmore8505@gmail.com>
2022-04-05 19:58:03 +0530

    MODIFIED: Checking option for No Play,Snake and Ladder

---
+++ src/com/infogalaxy/Constants.java
@@ -0,0 +1,8 @@
+package com.infogalaxy;
+
+public class Constants {
+    //Define Constant variables for Option
+    public static final int NO_PLAY= 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
+}
--- src/com/infogalaxy/SnakeAndLaddersSimulator.java
+++ src/com/infogalaxy/SnakeAndLaddersSimulator.java
@@ -3,21 +3,55 @@ package com.infogalaxy;
 import java.util.Random;
 
 public class SnakeAndLaddersSimulator {
+    int position = 0;
+    int dice;
+
+    //Showing Current Position of player
     public void position() {
-        int position = 0;
+
         System.out.println("Players Position is = " + position);
     }
 
-    public void rollsDie() {
-        Random random = new Random();
-        int dies = (int) (Math.random() * (6 - 1) + 1);
-        System.out.println("Die Number is = " + dies);
+    //Rollling the Dice to Get Position
+    public void rollsDice() {
+        dice = (int) (Math.random() * (6 - 1) + 1);
+        System.out.println("Dice Number is = " + dice);
+    }
+
+    //UC-3-Checking for Option to Play
+    public void checkOption() {
+        int option = (int) ((Math.random() * 3 - 1) + 1);
+        System.out.println("Option Number = " + option);
+        Constants constants = new Constants();
+        switch (option) {
+            case Constants.NO_PLAY:
+                System.out.println("No Play Arrived. Stay Same Position...");
+                break;
+            case Constants.LADDER:
+                System.out.println("HURRAY..! U Got Ladder..");
+                position = position + dice;
+                break;
+            case Constants.SNAKE:
+                if (position < 0) {
+                    position = -(position);
+                }
+                System.out.println("OOPS.. U Got Snake..");
+                break;
+        }
     }
 
     public static void main(String[] args) {
+        //Creating Snake and Laader Object
         SnakeAndLaddersSimulator player1 = new SnakeAndLaddersSimulator();
 
+        //Showing Snake and Ladder Position
         player1.position();
-        player1.rollsDie();
+
+        //Rollinng the Dice
+        player1.rollsDice();
+
+        //Checking Option
+        player1.checkOption();
+        player1.position();
     }
 }
