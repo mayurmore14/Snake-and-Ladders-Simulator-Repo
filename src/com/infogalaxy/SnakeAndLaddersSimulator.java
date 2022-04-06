342e020fb76eac0346901c563db2a7f0f1903c59
mayurmore14 <mayurmore8505@gmail.com>
2022-04-06 18:35:27 +0530

    MODIFIED: Repeate Till the Player Reaches Winning Position

--- src/com/infogalaxy/SnakeAndLaddersSimulator.java
+++ src/com/infogalaxy/SnakeAndLaddersSimulator.java
@@ -1,57 +1,59 @@
 package com.infogalaxy;
 
-import java.util.Random;
 
 public class SnakeAndLaddersSimulator {
+
     int position = 0;
     int dice;
 
-    //Showing Current Position of player
+    //UC-1-Showing Current Position of player
     public void position() {
 
         System.out.println("Players Position is = " + position);
     }
 
-    //Rollling the Dice to Get Position
+    //UC-2-Rollling the Dice to Get No Between 1 to 6
     public void rollsDice() {
-        dice = (int) (Math.random() * (6 - 1) + 1);
-        System.out.println("Dice Number is = " + dice);
-    }
+        //UC-4-Repeate Till the Player Reaches Winning Position 100
+        while (position != 100) {
+            dice = (int) ((Math.random() * (7 - 1)) + 1);
+            System.out.println("Dice Number is = " + dice);
 
-    //UC-3-Checking for Option to Play
-    public void checkOption() {
-        int option = (int) ((Math.random() * 3 - 1) + 1);
-        System.out.println("Option Number = " + option);
-        Constants constants = new Constants();
-        switch (option) {
-            case Constants.NO_PLAY:
-                System.out.println("No Play Arrived. Stay Same Position...");
-                break;
-            case Constants.LADDER:
-                System.out.println("HURRAY..! U Got Ladder..");
-                position = position + dice;
-                break;
-            case Constants.SNAKE:
-                if (position < 0) {
-                    position = -(position);
-                }
-                System.out.println("OOPS.. U Got Snake..");
-                break;
+            //UC-3-Checking for Option to Play
+            int option = (int) ((Math.random() * (3 - 1)) + 1);
+            System.out.println("Option Number = " + option);
+            Constants constants = new Constants();
+            switch (option) {
+                case Constants.NO_PLAY:
+                    System.out.println("No Play Arrived. Stay Same Position...");
+                    break;
+                case Constants.LADDER:
+                    System.out.println("HURRAY..! U Got Ladder..");
+                    position = position + dice;
+                    if (position > 100) {
+                        System.out.println("Player Position is out of board..");
+                        position = position - dice;
+                    }
+                    break;
+                case Constants.SNAKE:
+                    if (position < 0) {
+                        position = -(position);
+                    }
+                    System.out.println("OOPS.. U Got Snake..");
+                    break;
+            }
+            position();
         }
     }
 
     public static void main(String[] args) {
-        //Creating Snake and Laader Object
+        //Creating Snake and Ladder Object
         SnakeAndLaddersSimulator player1 = new SnakeAndLaddersSimulator();
 
         //Showing Snake and Ladder Position
         player1.position();
-
         //Rollinng the Dice
         player1.rollsDice();
 
-        //Checking Option
-        player1.checkOption();
-        player1.position();
     }
 }
